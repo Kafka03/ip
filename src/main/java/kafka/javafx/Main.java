86b7c639a3601bc7841d3274d5966ffa1d7a0047
Kafka <e1354240@u.nus.edu>
2026-08-28 22:49:22 +0800

    Add images for GUI

--- src/main/java/kafka/javafx/Main.java
+++ src/main/java/kafka/javafx/Main.java
@@ -5,12 +5,19 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import kafka.javafx.components.DialogBox;
 
-
+/**
+ * Displays the JavaFX user interface for Kafka.
+ */
 public class Main extends Application {
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
     private ScrollPane scrollPane;
     private VBox dialogContainer;
@@ -20,22 +27,25 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-         //Setting up required components
+        // Set up required components
 
-         scrollPane = new ScrollPane();
-         dialogContainer = new VBox();
-         scrollPane.setContent(dialogContainer);
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
 
-         userInput = new TextField();
-         sendButton = new Button("Send");
+        userInput = new TextField();
+        sendButton = new Button("Send");
 
-         AnchorPane mainLayout = new AnchorPane();
-         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+        DialogBox dialogBox = new DialogBox("Hello!", userImage);
+        dialogContainer.getChildren().addAll(dialogBox);
 
-         scene = new Scene(mainLayout);
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
 
-         stage.setScene(scene);
-         stage.show();
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
 
--- src/main/java/kafka/javafx/components/DialogBox.java
+++ src/main/java/kafka/javafx/components/DialogBox.java
@@ -1,19 +1,27 @@
 package kafka.javafx.components;
 
-import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
+/**
+ * Displays a message together with its associated profile image.
+ */
 public class DialogBox extends HBox {
 
     private Label text;
     private ImageView displayPicture;
 
-    public DialogBox(String s, Image i) {
-        text = new Label(s);
-        displayPicture = new ImageView(i);
+    /**
+     * Creates a dialog box containing the specified message and image.
+     *
+     * @param message message to display
+     * @param image profile image to display beside the message
+     */
+    public DialogBox(String message, Image image) {
+        text = new Label(message);
+        displayPicture = new ImageView(image);
         this.getChildren().addAll(text, displayPicture);
     }
 }
